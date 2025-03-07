7706bb0a25cf6fed920502b33d97379ed37a86c8
Gena <shindyapingena@gmail.com>
2025-03-07 17:21:06 +0400

    fetch-commit

--- src/main/java/mentorship/roadmap/step_2_core/step2/string/StringLength/StringLength.java
+++
@@ -1,9 +0,0 @@
-package mentorship.roadmap.step_2_core.step2.string.StringLength;
-
-public class StringLength {
-    public static void main(String[] args) {
-        String text = "Java Programming";
-        int length = text.length();
-        System.out.println("Длина строки: " + length);
-    }
-}
