1b902f499461e06b64b800011e6de903001e2bbb
TryToGetRest <shindyapingena@gmail.com>
2025-03-12 00:52:44 +0400

    packaging branches

--- src/main/java/mentorship/roadmap/Java_Core/step7_StreamApi/topic1_LambdaExpressions/task4_Filter/solution/MyPredicate.java
+++
@@ -1,6 +0,0 @@
-package mentorship.roadmap.Java_Core.step7_StreamApi.topic1_LambdaExpressions.task4_Filter.solution;
-
-@FunctionalInterface
-public interface MyPredicate<T> {
-    boolean test(T t);
-}
