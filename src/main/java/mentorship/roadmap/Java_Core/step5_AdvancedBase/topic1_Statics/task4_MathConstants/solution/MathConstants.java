680735d4f5d2b9b27e8140264fb42377cb1f9d98
TryToGetRest <shindyapingena@gmail.com>
2025-03-12 00:55:09 +0400

    packaging branches

--- src/main/java/mentorship/roadmap/Java_Core/step5_AdvancedBase/topic1_Statics/task4_MathConstants/solution/MathConstants.java
+++
@@ -1,7 +0,0 @@
-package mentorship.roadmap.Java_Core.step5_AdvancedBase.topic1_Statics.task4_MathConstants.solution;
-
-public class MathConstants {
-
-    public static final double PI = 3.14159;
-    public static final double E = 2.71828;
-}
