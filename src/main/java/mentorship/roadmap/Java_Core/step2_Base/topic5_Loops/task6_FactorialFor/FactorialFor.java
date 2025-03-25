053a5d8fec4845bad038d9dd90c8b404915e0e26
TryToGetRest <shindyapingena@gmail.com>
2025-03-25 12:07:06 +0400

    fix factorialFor

--- src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic5_Loops/task6_FactorialFor/FactorialFor.java
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic5_Loops/task6_FactorialFor/FactorialFor.java
@@ -3,7 +3,7 @@ package mentorship.roadmap.Java_Core.step2_Base.topic5_Loops.task6_FactorialFor;
 public class FactorialFor {
 
     public static void main(String[] args) {
-        int n = 15;
+        int n = 11;
         int factorial = 1;
         for (int i = 1; i <= n; i++) {
             factorial *= i;
