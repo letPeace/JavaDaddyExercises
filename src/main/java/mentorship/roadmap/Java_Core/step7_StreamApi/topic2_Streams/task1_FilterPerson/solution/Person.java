0d3ce72bd10e9dc7557a4a620e5dcad623cf2f60
Gennadiy Shindyapin <shindyapingena@gmail.com>
2025-07-31 22:12:40 +0300

    Новые задачи, удалены решения

--- src/main/java/mentorship/roadmap/Java_Core/step7_StreamApi/topic2_Streams/task1_FilterPerson/solution/Person.java
+++
@@ -1,4 +0,0 @@
-package mentorship.roadmap.Java_Core.step7_StreamApi.topic2_Streams.task1_FilterPerson.solution;
-
-public record Person(String name, Integer age) {
-}
--- src/main/java/mentorship/roadmap/Java_Core/step7_StreamApi/topic2_Streams/task9_PersonToMap/solution/Person.java
+++
@@ -1,4 +0,0 @@
-package mentorship.roadmap.Java_Core.step7_StreamApi.topic2_Streams.task9_PersonToMap.solution;
-
-public record Person(String name, Integer age) {
-}
