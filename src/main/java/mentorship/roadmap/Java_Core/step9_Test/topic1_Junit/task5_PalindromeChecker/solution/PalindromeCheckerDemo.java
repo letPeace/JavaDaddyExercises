dd34149bdf1609f2b96715503974749b2164f6c0
Gennadiy Shindyapin <shindyapingena@gmail.com>
2025-07-31 22:17:25 +0300

    Новые задачи, удалены решения

--- src/main/java/mentorship/roadmap/Java_Core/step9_Test/topic1_Junit/task5_PalindromeChecker/solution/PalindromeCheckerDemo.java
+++
@@ -1,9 +0,0 @@
-package mentorship.roadmap.Java_Core.step9_Test.topic1_Junit.task5_PalindromeChecker.solution;
-
-public class PalindromeCheckerDemo {
-    public static boolean isPalindrome(String text) {
-        String cleanText = text.replaceAll("[^a-zA-Z]", "").toLowerCase();
-        String reversed = new StringBuilder(cleanText).reverse().toString();
-        return cleanText.equals(reversed);
-    }
-}
