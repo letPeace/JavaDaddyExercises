df025d094dfb43faac2a87170540407562caf22c
valeriya <valeriyaakse@gmail.com>
2025-08-14 11:48:33 +0300

    add HelloWorld task

---
+++ src/main/java/mentorship/roadmap/Java_Core/step2_Base/topic1_Console/task1_HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+    static void helloWorld() {
+        System.out.println("Hello, World!");
+    }
+}
